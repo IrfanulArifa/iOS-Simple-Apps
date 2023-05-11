2de3e5a5e9a30451ea71e2f48e0e23983d9d4ff9
IrfanulArifa <57009203+IrfanulArifa@users.noreply.github.com>
2023-05-11 08:56:53 +0700

    Initial Commit

---
+++ Portal Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Portal Game
+//
+//  Created by Irfanul Arifa on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
